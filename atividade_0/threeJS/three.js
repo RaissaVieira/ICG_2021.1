5e72c33163d7c2471c28fc9fc5ff8b8cf2dbc313
Raissa Vieira <raissa.vieira@lavid.ufpb.br>
2021-08-22 21:40:44 -0300

    feat: Adicionando questao 4.3

--- atividade_0/threeJS/three.js
+++ atividade_0/threeJS/three.js
@@ -62,43 +62,111 @@ function RotationCube() {
 // Questao 4.3
 
 function ThreeCubes() {
-    
+
     const scene = new THREE.Scene();
     renderer.render( scene, camera );
-
+    
     const geometry = new THREE.BoxGeometry();
     const material = [
-        new THREE.MeshBasicMaterial({ color: 0x7fb069 }),
-        new THREE.MeshBasicMaterial({ color: 0xb540bf }),
-        new THREE.MeshBasicMaterial(),
+        new THREE.MeshBasicMaterial( { color: 0x654684 } ),
+        new THREE.MeshLambertMaterial( { color: 0x568726 } ),
+        new THREE.MeshPhysicalMaterial ( {color: 0xEAEBEA } ),
     ];
     
-    const cube = [
-        new THREE.Mesh(geometry, material[0]),
-        new THREE.Mesh(geometry, material[1]),
-        new THREE.Mesh(geometry, material[2])
+    const cubes = [
+        new THREE.Mesh( geometry, material[0] ),
+        new THREE.Mesh( geometry, material[1] ),
+        new THREE.Mesh( geometry, material[2] ),
     ];
 
-    for(var i=0; i<3; i++) scene.add(cube[i]);
+    scene.add( cubes[0] );
+    scene.add( cubes[1] );
+    scene.add( cubes[2] );
 
-    cube[0].position.set(2,1,0);
-    cube[0].rotation.set(1,0.5,0);
+    camera.position.set(2, 2, 5);
+    camera.rotation.set(-0.2, 0.2, 0);
 
-    cube[1].position.set(-2,-1,0);
-    cube[1].rotation.set(1,0.5,0);
+    cubes[0].position.x = -2.5;
+    cubes[1].position.x = 0;
+    cubes[2].position.x = 2;
 
-    cube[2].position.set(0,0,0);
-    cube[2].rotation.set(1,0.5,0);
+    cubes[0].position.y = 0.5;
+    cubes[1].position.y = -0.3;
+    cubes[2].position.y = 0.5;
 
-    camera.position.z = 5;
+    const ambient_light = new THREE.AmbientLight(0x404040, 0.3); // soft white ambient light
+    const directional_light = new THREE.DirectionalLight(0xffffff, 1); // white directional light
+
+    directional_light.position.set(0, 2, 2);
+    directional_light.castShadow = true;
+
+    scene.add(ambient_light);
+    scene.add(directional_light);
 
     function animate() {
         requestAnimationFrame( animate );
+        
+        renderer.render( scene, camera );
+    }
 
-        console.log(scene)
+    animate();
+}
+
+function RotationThreeCubes() {
+
+    const scene = new THREE.Scene();
+    renderer.render( scene, camera );
+    
+    const geometry = new THREE.BoxGeometry();
+    const material = [
+        new THREE.MeshBasicMaterial( { color: 0x654684 } ),
+        new THREE.MeshLambertMaterial( { color: 0x568726 } ),
+        new THREE.MeshPhysicalMaterial ( {color: 0xEAEBEA } ),
+    ];
+    
+    const cubes = [
+        new THREE.Mesh( geometry, material[0] ),
+        new THREE.Mesh( geometry, material[1] ),
+        new THREE.Mesh( geometry, material[2] ),
+    ];
+
+    scene.add( cubes[0] );
+    scene.add( cubes[1] );
+    scene.add( cubes[2] );
+
+    camera.position.set(2, 2, 5);
+    camera.rotation.set(-0.2, 0.2, 0);
+
+    cubes[0].position.x = -2.5;
+    cubes[1].position.x = 0;
+    cubes[2].position.x = 2;
+
+    cubes[0].position.y = 0.5;
+    cubes[1].position.y = -0.3;
+    cubes[2].position.y = 0.5;
+
+    const ambient_light = new THREE.AmbientLight(0x404040, 0.3); // soft white ambient light
+    const directional_light = new THREE.DirectionalLight(0xffffff, 1); // white directional light
+
+    directional_light.position.set(0, 2, 2);
+    directional_light.castShadow = true;
+
+    scene.add(ambient_light);
+    scene.add(directional_light);
+
+    function animate() {
+
+        requestAnimationFrame( animate );
+
+        for (let i = 0; i < cubes.length; i++) {
+        cubes[i].rotation.x += 0.01;
+        cubes[i].rotation.y += 0.01;
+        }
 
         renderer.render( scene, camera );
     }
 
     animate();
 }
+
+
